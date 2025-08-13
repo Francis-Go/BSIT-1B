11832d282d4b88193edee33530c58e5231d7479a
Francis Go <gofrancisjanaaron@gmail.com>
2025-08-13 15:05:23 +0800

    Delete GO_code_challenge1.py

--- GO_code_challenge1.py
+++
@@ -1,2 +0,0 @@
-name = input("Type your name -->")
-print(" \n\t\t\t\t\t\t * \n\t\t\t\t\t       *   * \n\t\t\t\t\t     *       * \n\t\t\t\t\t   *           * \n\t\t\t\t         *      Hi       * \n\t\t\t\t         *  ", name,    "   * \n\t\t\t\t\t   *           * \n\t\t\t\t\t     *       * \n\t\t\t\t\t       *   * \n\t\t\t\t\t\t * ")
