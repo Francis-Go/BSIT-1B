2c94575ad73c7a3e17e24df6614dcc413fcd5775
Francis Go <gofrancisjanaaron@gmail.com>
2025-08-27 15:12:08 +0800

    Add files via upload

---
+++ GO_Activity10.py
@@ -0,0 +1,13 @@
+username = 'Francis'
+password = 'Go123'
+
+usernameinput = input("Type your username: " )
+passwordinput = input("Type your password: " )
+
+if (username == usernameinput) and (password == passwordinput):
+	print("Login succesfully")
+else:
+	print("Login failed")
+
+#if (username != 'Francis') or (password != 'Go123'):
+#	print("Login failed")
