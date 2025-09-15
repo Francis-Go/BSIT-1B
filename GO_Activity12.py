c8d6d714e6df44c06f2c6704f076eec7d8ba51c0
Francis Go <gofrancisjanaaron@gmail.com>
2025-09-15 15:08:52 +0800

    Add files via upload

---
+++ GO_Activity12.py
@@ -0,0 +1,2 @@
+for Denver in range(2, 201, 2):
+    print(Denver, "I love Cuarez")
---
+++ GO_Activity13.py
@@ -0,0 +1,6 @@
+sum = 0
+for number in range(5):
+    number_input = eval(input("Enter the number here > " ))
+    sum += number
+print("Here is the total summation of your inputted number:", sum)
+
