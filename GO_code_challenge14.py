148481d95dd8ffd7f00091db037f62456933c547
Francis Go <gofrancisjanaaron@gmail.com>
2025-10-26 23:51:19 +0800

    Update GO_code_challenge14.py

--- GO_code_challenge14.py
+++ GO_code_challenge14.py
@@ -1,11 +1,11 @@
 print("Welcome to the anime lister.")
 insert = ""
 anime_list = []
-
 while insert != "done":
     insert = input("Enter the title of an anime (type 'done' to finish): " )
     anime_list.append(insert)
     print("'", insert, "' has been added to the list")
+anime_list.pop()
 print("You have finished listing your anime.")
 print(f"Your anime list includes the following shows:")
 for show in anime_list:
